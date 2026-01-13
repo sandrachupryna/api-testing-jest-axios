cc267e9421f399c341bdae45a0da8691f4ebfe50
Oleksandra Chupryna <oleksandra.chupryna.work@gmail.com>
2026-01-14 01:58:01 +0200

    add faker for test data generation

--- src/data/testData.js
+++ src/data/testData.js
@@ -1,16 +1,18 @@
+const { faker } = require('@faker-js/faker');
+
 const testData = {
     validPost: {
         userId: 1,
-        body: 'This is a test post body.',
-        title: 'Test Post Title',
-        publishedAt: "14/01/2026 13:25:21"
+        title: faker.lorem.sentence(5),
+        body: faker.lorem.text(),
+        publishedAt: faker.date.recent().toISOString()
     },
 
     updatePost: {
         userId: 1,
-        body: 'This is an updated test post body.',
-        title: 'Updated Test Post Title',
-        publishedAt: "14/01/2026  15:15:30"
+        title: faker.lorem.sentence(5),
+        body: faker.lorem.text(),
+        publishedAt: faker.date.recent().toISOString()
     }
 }
 
