dadababf7335621578de1c04acc23ad1fb0e95f5
Oleksandra Chupryna <oleksandra.chupryna.work@gmail.com>
2026-01-14 18:28:02 -0300

    add task2: requests with custom params and headers

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,11 +1,12 @@
 const httpClient = require('./httpClient');
 const endpoints = require('../config/endpoints');
 const httpErrorHandler = require('../utils/httpErrorHandler');
+const { he } = require('@faker-js/faker');
 
 class ApiService {
-  async #get(url, params = {}) {
+  async #get(url, params = {}, headers = {}) {
     try {
-      const response = await httpClient.get(url, { params });
+      const response = await httpClient.get(url, { params, headers });
       return response;
     } catch (error) {
       return httpErrorHandler(error);
@@ -39,8 +40,8 @@ class ApiService {
     }
   }
   
-  async getAllPosts() {
-    return this.#get(endpoints.posts);
+  async getAllPosts(params = {}, headers = {}) {
+    return this.#get(endpoints.posts, params, headers);
   }
 
   async getPostById(id) {
