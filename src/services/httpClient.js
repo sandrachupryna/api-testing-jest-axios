1ca255cd6695a7706d25f51a8e355643e05165b3
Oleksandra Chupryna <oleksandra.chupryna.work@gmail.com>
2026-01-14 14:19:04 +0200

    add interceptors for request and response logging

--- src/services/httpClient.js
+++ src/services/httpClient.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const config = require('../config/config');
+const chalk = require('chalk');
 
 // Створюємо Axios instance з базовою конфігурацією
 const httpClient = axios.create({
@@ -7,5 +8,39 @@ const httpClient = axios.create({
   timeout: config.timeout,
   headers: config.defaultHeaders
 });
+
+// Interceptor for Requests
+httpClient.interceptors.request.use(
+  (request) => {
+    console.log(chalk.blueBright.bold(`[REQUEST] ${request.method.toUpperCase()} ${request.url}`));
+    if (request.params) {
+      console.log(chalk.yellowBright.bold(`[REQUEST PARAMS]`));
+      console.log(chalk.yellow(JSON.stringify(request.params, null, 2)));
+    }
+    if (request.data) {
+      console.log(chalk.cyanBright.bold(`[REQUEST DATA]`));
+      console.log(chalk.cyan(JSON.stringify(request.data, null, 2)));
+    } 
+    return request;
+  },
+  (error) => {
+    console.error(chalk.redBright.bold('[REQUEST ERROR]', error));
+    return Promise.reject(error);
+  }
+)
+
+// Interceptor for Responses
+httpClient.interceptors.response.use(
+  (response) => {
+    console.log(chalk.greenBright.bold(`[RESPONSE] ${response.status} ${response.config.url}`));
+    console.log(chalk.cyanBright.bold(`[RESPONSE DATA]`));
+    console.log(chalk.cyan(JSON.stringify(response.data, null, 2)));
+    return response;
+  },
+  (error) => {
+    console.error(chalk.redBright.bold('[RESPONSE ERROR]', error));
+    return Promise.reject(error);
+  }
+)
 
 module.exports = httpClient;
